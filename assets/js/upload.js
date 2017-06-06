b1fd12d06338f2160f57508f7b5277280c4a7525
Thiago A. Klein <klein.thiago@gmail.com>
2017-06-06 08:49:36 -0300

    Melhorias em JS

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -1,8 +1,9 @@
 var SoftUpload = {
 
   upload: document.getElementById("upload"),
-  step1: document.querySelector(".step1"),
-  step2: document.querySelector(".step2"),
+  step1:  document.querySelector(".step1"),
+  step2:  document.querySelector(".step2"),
+  delays: [1,2,8,5,10],
 
   initialize: function() {
     SoftUpload.bindEvents();
@@ -27,7 +28,8 @@ var SoftUpload = {
       SoftUpload.preview(files[i]);
     }
 
-    // TODO: aqui deveriamos colocar um ajax para salvar o arquivo no servidor
+    // TODO: Aqui deveriamos colocar um ajax para salvar o arquivo no servidor
+    // @thiago
 
   },
 
@@ -35,13 +37,32 @@ var SoftUpload = {
     var reader = new FileReader();
 
     reader.onload = function (event) {
-      var node = document.createElement("li");
-      var div = document.createElement("div");
-      var filename = document.createTextNode("Arquivo Fonte do site.zip");
-      div.className = "progress";
-      div.appendChild(filename);
+      var node     = document.createElement("li");
+      var span     = document.createElement("span");
+      var bg       = document.createElement("div"); bg.className  = "bg";
+      var div      = document.createElement("div"); div.className = "progress";
+      var filename = document.createTextNode(file.name);
+
+      span.appendChild(filename);
+      div.appendChild(bg).appendChild(span);
       node.appendChild(div);
       document.getElementById("files-list").append(node);
+
+      // Aqui usei um cálculo para simular um upload (por não tratar a requisição de envio ao servidor)
+      // O cálculo certo é baseado no file.size!
+      // @thiago
+
+      var width = 0;
+      var fakeUpload = setInterval(function() {
+        var random = SoftUpload.delays[Math.floor(Math.random() * SoftUpload.delays.length)]
+        if((width + random) > 100)
+          width = 100;
+        else
+          width += random;
+        bg.style.width = width + "%"
+        if(width >= 100) clearInterval(fakeUpload);
+      }, 300);
+
     };
 
     reader.readAsDataURL(file);
