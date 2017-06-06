98f10cfbcb544617215ad2e736e923e74b18b0ca
Thiago A. Klein <klein.thiago@gmail.com>
2017-06-06 14:16:59 -0300

    Melhorias nas barras

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -19,6 +19,13 @@ var SoftUpload = {
       SoftUpload.step2.classList.remove("hidden");
       SoftUpload.read(e.dataTransfer.files);
     }
+
+    document.querySelector(".more-files").addEventListener("click", SoftUpload.backToFirstStep);
+    document.querySelector(".close-import").addEventListener("click", SoftUpload.backToFirstStep);
+  },
+
+  backToFirstStep: function() {
+    location.reload();
   },
 
   read: function(files) {
@@ -28,9 +35,7 @@ var SoftUpload = {
       SoftUpload.preview(files[i]);
     }
 
-    // TODO: Aqui deveriamos colocar um ajax para salvar o arquivo no servidor
-    // @thiago
-
+    // TODO: Aqui deveriamos colocar um ajax para salvar o arquivo no servidor - @thiago
   },
 
   preview: function(file) {
@@ -41,16 +46,17 @@ var SoftUpload = {
       var span     = document.createElement("span");
       var bg       = document.createElement("div"); bg.className  = "bg";
       var div      = document.createElement("div"); div.className = "progress";
+      var circle   = document.createElement("div"); circle.className = "circle";
       var filename = document.createTextNode(file.name);
 
       span.appendChild(filename);
       div.appendChild(bg).appendChild(span);
+      div.appendChild(circle);
       node.appendChild(div);
       document.getElementById("files-list").append(node);
 
       // Aqui usei um cálculo para simular um upload (por não tratar a requisição de envio ao servidor)
-      // O cálculo certo é baseado no file.size!
-      // @thiago
+      // O cálculo certo é baseado no file.size! - @thiago
 
       var width = 0;
       var fakeUpload = setInterval(function() {
@@ -60,7 +66,10 @@ var SoftUpload = {
         else
           width += random;
         bg.style.width = width + "%"
-        if(width >= 100) clearInterval(fakeUpload);
+        if(width >= 100) {
+          clearInterval(fakeUpload);
+          div.querySelector(".circle").className = "circle white"
+        }
       }, 300);
 
     };
